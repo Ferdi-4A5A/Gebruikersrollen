e744de812952f503d1a3ef146cb54bccc1b7a95a
Ferdi-4A5A <lolstfu@msn.com>
2017-06-07 20:46:00 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,60 +1,33 @@
-<!DOCTYPE html>
-<!--
-Template Name: Cooban
-Author: <a href="http://www.os-templates.com/">OS Templates</a>
-Author URI: http://www.os-templates.com/
-Licence: Free to use under our free template licence terms
-Licence URI: http://www.os-templates.com/template-terms
--->
-<html>
-<head>
-<title>Cooban | Pages | Full Width</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
-</head>
-<body id="top">
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- Top Background Image Wrapper -->
-<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/01.png');"> 
-  <!-- ################################################################################################ -->
-  <div class="row1">
-    <header id="header" class="hoc clear"> 
-      <!-- ################################################################################################ -->
-      <div id="logo" class="fl_left">
-        <h1><a href="index.html">Cooban</a></h1>
-      </div>
-      <nav id="mainav" class="fl_right">
-        <ul class="clear">
-          <li class="active"><a href="index.php">Home</a></li>
-          <li><a class="drop" href="#">Voorraden</a>
-            <ul>
-              <li><a href="voorraad.php">Voorraden</a></li>
-              <li><a href="producten.php">Producten</a></li>
-            </ul>
-          </li>
-          <li><a href="locaties.php">Locaties</a></li>
-          <li><a href="gebruikers.php">Gebruikers</a></li>
-        </ul>
-      </nav>
-      <!-- ################################################################################################ -->
-    </header>
-  </div>
-  <!-- ################################################################################################ -->
-  <!-- ################################################################################################ -->
-  <!-- ################################################################################################ -->
-  <div id="breadcrumb" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <!-- ################################################################################################ -->
-  </div>
-  <!-- ################################################################################################ -->
-</div>
-<!-- End Top Background Image Wrapper -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
+<?php
+include 'header.php';
+
+$conn = new PDO("mysql:host=192.168.56.1;dbname=gebruikersrollen", 'mainuser', 'dev01dev');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$stmt = $conn->prepare("SELECT userName, userPassword FROM users");
+$stmt->execute();
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_POST['btn-login'])) {
+  $stmt = $conn->prepare("SELECT userName, userPassword, userFunction FROM users WHERE userName = '".$_POST['userName']."' AND userPassword = '".$_POST['userPassword']."'");
+  $stmt->execute();
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  if (count($result) === 1) {
+    $_SESSION['userName'] = $_POST['userName'];
+    $_SESSION['userPassword'] = $_POST['userPassword'];
+    $_SESSION['userFunction'] = $_POST['userFunction'];
+
+    foreach ($result as $row) {
+      $_SESSION['userFunction'] = $row['userFunction'];
+    }
+  } else {
+    echo 'Wrong username/password combination.';
+  }
+
+}
+?>
+
 <div class="wrapper row3">
   <main class="hoc container clear"> 
     <!-- main body -->
@@ -82,15 +55,15 @@ Licence URI: http://www.os-templates.com/template-terms
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
 <div class="wrapper row4">
-  <footer id="footer" class="hoc clear"> 
+  <footer id="footer" class="hoc clear">
     <!-- ################################################################################################ -->
     <div class="one_quarter first">
       <h6 class="title">Phasellus aenean</h6>
       <address class="btmspace-15">
-      Company Name<br>
-      Street Name &amp; Number<br>
-      Town<br>
-      Postcode/Zip
+        Company Name<br>
+        Street Name &amp; Number<br>
+        Town<br>
+        Postcode/Zip
       </address>
       <ul class="nospace">
         <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
@@ -126,24 +99,5 @@ Licence URI: http://www.os-templates.com/template-terms
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
-<div class="wrapper row5">
-  <div id="copyright" class="hoc clear"> 
-    <!-- ################################################################################################ -->
-    <p class="fl_left">Copyright &copy; 2015 - All Rights Reserved - <a href="#">Domain Name</a></p>
-    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
-    <!-- ################################################################################################ -->
-  </div>
-</div>
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<!-- ################################################################################################ -->
-<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
-<!-- JAVASCRIPTS -->
-<script src="layout/scripts/jquery.min.js"></script>
-<script src="layout/scripts/jquery.backtotop.js"></script>
-<script src="layout/scripts/jquery.mobilemenu.js"></script>
-<!-- IE9 Placeholder Support -->
-<script src="layout/scripts/jquery.placeholder.min.js"></script>
-<!-- / IE9 Placeholder Support -->
-</body>
-</html>
+
+<?php include 'footer.php' ?>
